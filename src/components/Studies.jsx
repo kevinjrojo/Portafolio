0ebefde23240e832167d5f84cb12ec0e511fd361
kevin <kevin.rojokevin17@gmail.com>
2024-11-28 20:01:21 -0300

    completando certificados

--- src/components/Studies.jsx
+++ src/components/Studies.jsx
@@ -1,3 +1,4 @@
+import "../styles/studies.css";
 import html from "../assets/icon/html-5.jpg";
 import css from "../assets/icon/css-3.jpg";
 import javascrip from "../assets/icon/js.jpg";
@@ -6,8 +7,21 @@ import node from "../assets/icon/node js.jpg";
 import bootstrap from "../assets/icon/bootstrap.jpg";
 import sass from "../assets/icon/sass.jpg";
 import github from "../assets/icon/github.jpg";
+import desarrolorWebCertificates from "../assets/certificates/Desarrollo web.jpg";
+import javascripCertificates from "../assets/certificates/javaScript.jpg";
+import reactCertificates from "../assets/certificates/React js.jpg";
+import frontEndCertificates from "../assets/certificates/Front-End.jpg";
+import { useState } from "react";
 
 export const Studies = () => {
+  const [isZoomed, setIsZoomed] = useState(false); // Controla si la imagen está ampliada
+
+  const handleImageClick = () => {
+    setIsZoomed(true); // Establece la imagen seleccionada
+  };
+  const closeZoom = () => {
+    setIsZoomed(false); // Cierra el modo ampliado
+  };
   return (
     <div className="conteiner-studies">
       <h3>Skills</h3>
@@ -17,15 +31,55 @@ export const Studies = () => {
         nunca me olvido de trabajar con herramientas como Sass, Gulp, Git o
         GitHub.
       </p>
-      <div className="icon-image-studies">
-        <img src={html} alt="html" />
-        <img src={css} alt="" />
-        <img src={javascrip} alt="" />
-        <img src={react} alt="" />
-        <img src={node} alt="" />
-        <img src={bootstrap} alt="" />
-        <img src={sass} alt="" />
-        <img src={github} alt="" />
+      <div className="conteiner-icon-studies">
+        <img src={html} alt="logo-html" className="logo-studies-html" />
+        <img src={css} alt="logo-css" className="logo-studies-css" />
+        <img
+          src={javascrip}
+          alt="logo-javascript"
+          className="logo-studies-js"
+        />
+        <img src={react} alt="logo-react" className="logo-studies-react" />
+        <img src={node} alt="logo-nodejs" className="logo-studies-node" />
+        <img
+          src={bootstrap}
+          alt="logo-bootstrap"
+          className="logo-studies-bootstrap"
+        />
+        <img src={sass} alt="logo-sass" className="logo-studies-sass" />
+        <img src={github} alt="logo-github" className="logo-studies-github" />
+      </div>
+      <div className="conteiner-certificates">
+        <img
+          src={desarrolorWebCertificates}
+          alt="certificates"
+          className="certificates-studies"
+          onClick={handleImageClick}
+        />
+        {isZoomed && (
+          <div className="overlay" onClick={closeZoom}>
+            <img
+              src={desarrolorWebCertificates}
+              alt="Ampliada"
+              className="zoomed-image"
+            />
+          </div>
+        )}
+        <img
+          src={javascripCertificates}
+          alt="certificates"
+          className="certificates-studies"
+        />
+        <img
+          src={reactCertificates}
+          alt="certificates"
+          className="certificates-studies"
+        />
+        <img
+          src={frontEndCertificates}
+          alt="certificates"
+          className="certificates-studies"
+        />
       </div>
     </div>
   );
