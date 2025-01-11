f5b6aa4e35cb1f4ea68d83b8d097038d2400d397
kevin <kevin.rojokevin17@gmail.com>
2025-01-11 15:17:28 -0300

    Presentacion editada

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -12,18 +12,16 @@ export const Information = () => {
           <p className="information-text">
             <h2>Sobre mi</h2>
             ¡Hola! Soy Kevin, un apasionado desarrollador front-end con
-            experiencia en la creación de experiencias web excepcionales. Con
-            más de 1 años de experiencia en el campo, me especializo en
-            convertir diseños creativos en interfaces interactivas y
-            funcionales. <br />
+            experiencia en la creación de páginas web excepcionales. Con más de
+            2 años de experiencia en el campo, me especializo en convertir
+            diseños creativos en interfaces interactivas y funcionales. <br />
             <br /> He trabajado en proyectos emocionantes que van desde
             aplicaciones web responsivas hasta plataformas de comercio
             electrónico de alto rendimiento. Estoy familiarizado con una
             variedad de tecnologías front-end, incluyendo HTML5, CSS3,
-            JavaScript y frameworks modernos como React.js. También tengo
-            experiencia en el uso de herramientas de desarrollo como Webpack y
-            Babel para optimizar el rendimiento y la compatibilidad del
-            navegador. <br /> <br />
+            JavaScript y frameworks modernos como React.js. También tengo una
+            básica experiencia en desarrolo Back-end con Node js y base de datos
+            no relacional con Mongo db. <br /> <br />
             Soy un solucionador de problemas creativo, siempre buscando nuevas
             formas de mejorar la experiencia del usuario y la eficiencia del
             código. Me enorgullece escribir código limpio, semántico y modular
