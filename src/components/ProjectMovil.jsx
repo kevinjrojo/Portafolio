a89d35de20cbd015abf9c46517c65b1146e1c3ce
kevin <kevin.rojokevin17@gmail.com>
2025-01-13 20:40:02 -0300

    grid project movil cambiado

--- src/components/ProjectMovil.jsx
+++ src/components/ProjectMovil.jsx
@@ -1,38 +1,45 @@
+import "../styles/project.css";
+
 export const ProjectMovil = ({ Projects }) => {
   return (
     <div className="projects-container-movil">
-      <strong className="title-movil">Proyectos</strong>
-      {Projects.map((Project, index) => (
-        <div key={index} className="target-project-movil">
-          <div
-            className="card-proyect-movil"
-            style={{
-              backgroundImage: `url(${Project.Image})`,
-            }}
-          >
-            <strong className="tilte-proyect-movil">{Project.Name}</strong>
-            <nav className="card_link_project_movil">
+      <nav className="conteiner-title-project">
+        <strong className="title-movil">Proyectos</strong>
+      </nav>
+      <main className="conteiner-project-card-movil">
+        {Projects.map((Project, index) => (
+          <div key={index}>
+            <div
+              className="card-proyect-movil"
+              style={{
+                backgroundImage: `url(${Project.Image})`,
+              }}
+            >
               {" "}
-              <a
-                href={Project.Link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="card__link_app_movil"
-              >
-                <strong>Ir a app</strong>
-              </a>
-              <a
-                href={Project.Link2}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="card__link_code_movil"
-              >
-                <strong>Ir a code</strong>
-              </a>
-            </nav>
+              <strong className="tilte-proyect-movil">{Project.Name}</strong>
+              <nav className="card_link_project_movil">
+                {" "}
+                <a
+                  href={Project.Link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="card__link_app_movil"
+                >
+                  <strong>Ir a app</strong>
+                </a>
+                <a
+                  href={Project.Link2}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="card__link_code_movil"
+                >
+                  <strong>Ir a code</strong>
+                </a>
+              </nav>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </main>
     </div>
   );
 };
