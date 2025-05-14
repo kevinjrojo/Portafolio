82faf0dc292049bd5329cdc588fd902472bee02b
kevin <kevin.rojokevin17@gmail.com>
2025-05-14 20:47:52 -0300

    eliminando imagen no utilizada y ajustando estilos en el componente Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,22 +1,20 @@
 import "../styles/intro.css";
-import { gsap } from "gsap";
-    
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-// ScrollSmoother requires ScrollTrigger
-import { ScrollSmoother } from "gsap/ScrollSmoother"
+
 import photo from "../assets/photo.webp";
+import photoMovil from "../assets/foto kevin 2.webp";
 
 export const Intro = () => {
-  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
   return (
     <div className="intro" data-aos="fade-down">
       <header>
         <img src={photo} alt="Kevin Rojo" className="photo" />
+         <img src={photoMovil} alt="Kevin Rojo" className="photo-movil" />
       </header>
       <article className="text-intro">
-        <p>¡Hola! mi nombre es</p>
+        <p >¡Hola! mi nombre es</p>
         <h1>KEVIN JAVIER ROJO</h1>
-        <p>y soy</p>
+        <p >y soy</p>
         <h1>FRONT-END DEVELOPER</h1>
 
      </article>
