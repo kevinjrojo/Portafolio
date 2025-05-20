7ac3ba3181900d53316d6bc0c1c5390017d82c88
kevin <kevin.rojokevin17@gmail.com>
2025-05-20 16:54:30 -0300

    mejorando la estructura y estilos del componente Project; ajustando la presentación de proyectos y eliminando descripciones innecesarias

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,23 +3,26 @@ import { projects } from "../service/data.js";
 
 export const Project = () => {
   return (
-    <main className="projects">
-      {projects.map((project, index) => (
-        <div key={index} className="project-card">
-          <div>
-            <h3>{project.Name}</h3>
-            <p>{project.Description}</p>
+    <div className="project-container">
+      <h2>Proyectos</h2>
+      <main className="projects">
+        {projects.map((project, index) => (
+          <div key={index} className="project-card">
             <div>
-              <a href={project.Link} target="_blank">
-                <strong>Ir a app</strong>
-              </a>
-              <a href={project.Link2} target="_blank">
-                <strong>Ir a code</strong>
-              </a>
+              <h3>{project.Name}</h3>
+              <p>{project.Description}</p>
+              <div>
+                <a href={project.Link} target="_blank">
+                  <strong>Ir a app</strong>
+                </a>
+                <a href={project.Link2} target="_blank">
+                  <strong>Ir a code</strong>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </main>
+        ))}
+      </main>
+    </div>
   );
 };
--- src/service/data.js
+++ src/service/data.js
@@ -1,16 +1,14 @@
 export const projects = [
   {
     Name: "Loop",
-    Description:
-      "Red social en desarrollo que permite a los usuarios compartir fotos, interactuar y conectar con amigos de manera sencilla.",
+
     Image: "../assets/projects/Loop.webp",
     Link: "https://kevinjrojo.github.io/Loop/",
     Link2: "https://github.com/kevinjrojo/Loop/",
   },
   {
     Name: "Generador de QR",
-    Description:
-      "Herramienta práctica para generar códigos QR personalizados, desarrollada con HTML, CSS, JavaScript y una API externa para la funcionalidad.",
+
     Image: "../assets/projects/qr.webp",
     Link: "https://kevinjrojo.github.io/Generador-QR/",
     Link2: "https://github.com/kevinjrojo/Generador-QR?tab=readme-ov-file",
@@ -18,24 +16,14 @@ export const projects = [
 
   {
     Name: "Citas aleatorias",
-    Description:
-      "Juego interactivo diseñado para encontrar palabras ocultas, ideal para mejorar habilidades lógicas y pasar un rato entretenido.",
+
     Image: "../assets/projects/citas-aleatorias.webp",
     Link: "https://kevinjrojo.github.io/Juego_Palabras/",
     Link2: "https://github.com/kevinjrojo/Juego_Palabras",
   },
   {
     Name: "Citas aleatorias",
-    Description:
-      "Juego interactivo diseñado para encontrar palabras ocultas, ideal para mejorar habilidades lógicas y pasar un rato entretenido.",
-    Image: "../assets/projects/citas-aleatorias.webp",
-    Link: "https://kevinjrojo.github.io/Juego_Palabras/",
-    Link2: "https://github.com/kevinjrojo/Juego_Palabras",
-  },
-  {
-    Name: "Citas aleatorias",
-    Description:
-      "Juego interactivo diseñado para encontrar palabras ocultas, ideal para mejorar habilidades lógicas y pasar un rato entretenido.",
+
     Image: "../assets/projects/citas-aleatorias.webp",
     Link: "https://kevinjrojo.github.io/Juego_Palabras/",
     Link2: "https://github.com/kevinjrojo/Juego_Palabras",
