b9dd531eee2d87bfd78b986a655093dc2586277b
kevin <kevin.rojokevin17@gmail.com>
2025-05-26 16:24:24 -0300

    Reemplazar íconos de imagen por íconos de react-icons en el componente Icon; eliminar archivos de íconos no utilizados.

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,23 +1,24 @@
 import "../styles/icon.css";
-import js from "../assets/icon/js.webp";
-import html from "../assets/icon/html.webp";
-import css from "../assets/icon/css.webp";
-import github from "../assets/icon/github.webp";
-import nodejs from "../assets/icon/nodejs.webp";
-import reactjs from "../assets/icon/reactjs.webp";
-import sass from "../assets/icon/sass.webp";
-import bootstrap from "../assets/icon/bootstrap.webp";
+
+import { SiCss3 } from "react-icons/si";
+import { SiGithub } from "react-icons/si";
+import { SiReact } from "react-icons/si";
+import { SiBootstrap } from "react-icons/si";
+import { SiSass } from "react-icons/si";
+import { SiNodedotjs } from "react-icons/si";
+import { SiJavascript } from "react-icons/si";
+import { SiHtml5 } from "react-icons/si";
 export const Icon = () => {
   return (
     <main className="icono">
-      <img src={js} alt="js" className="iconos infinite1" />
-      <img src={html} alt="js" className="iconos infinite2" />
-      <img src={css} alt="js" className="iconos infinite3" />
-      <img src={reactjs} alt="js" className="iconos infinite4" />
-      <img src={bootstrap} alt="js" className="iconos infinite5" />
-      <img src={nodejs} alt="js" className="iconos infinite6" />
-      <img src={sass} alt="js" className="iconos infinite7" />
-      <img src={github} alt="js" className="iconos infinite8" />
+      <SiJavascript className="iconos infinite1" />
+      <SiHtml5 className="iconos infinite2" />
+      <SiCss3 className="iconos infinite3" />
+      <SiReact className="iconos infinite4" />
+      <SiBootstrap className="iconos infinite5" />
+      <SiNodedotjs className="iconos infinite6" />
+      <SiSass className="iconos infinite7" />
+      <SiGithub className="iconos infinite8" />
     </main>
   );
 };
