3e3c8ed04c24e77d5ab6e9800c15c4cef6abe60a
kevin <kevin.rojokevin17@gmail.com>
2025-06-05 17:33:54 -0300

    Agregar íconos de redes sociales al formulario de contacto y mejorar la gestión de errores en el envío del formulario.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,11 @@ import "../styles/contact.css";
 import { FaUser } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { IoIosText } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { TbFileCv } from "react-icons/tb";
 
 export const Contact = () => {
   const [formData, setFormData] = useState({
@@ -10,7 +15,7 @@ export const Contact = () => {
     email: "",
     message: "",
   });
-
+  const [error, setError] = useState("");
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
@@ -30,11 +35,10 @@ export const Contact = () => {
       });
 
       if (res.ok) {
-        console.log("Formulario enviado con éxito");
-        alert("Email enviado exitosamente!");
         setFormData({ name: "", email: "", message: "" });
-      } else {
-        console.error("Error al enviar el formulario");
+      }
+      if (!formData.name || !formData.message || !formData.email) {
+        setError("completa los campos marcados");
       }
     } catch (error) {
       console.error("Error de red:", error);
@@ -48,12 +52,12 @@ export const Contact = () => {
         <form onSubmit={handleSubmit} className="form">
           <label htmlFor="Name">
             <FaUser className="contact-icon" />
+
             <input
               type="text"
               name="name"
               id="name"
               className="inputs"
-              required
               value={formData.name}
               onChange={handleChange}
               placeholder="Name"
@@ -78,7 +82,6 @@ export const Contact = () => {
               id="message-input"
               name="message"
               className="inputs"
-              required
               value={formData.message}
               onChange={handleChange}
             />
@@ -86,7 +89,15 @@ export const Contact = () => {
           <button type="submit" className="button">
             Enviar
           </button>
+          {error && <p style={{ color: "red" }}>{error}</p>}
         </form>
+      </section>
+      <section className="icon-container">
+        <FaInstagram className="icon-contact" />
+        <FaGithub className="icon-contact" />
+        <FaLinkedin className="icon-contact" />
+        <FaWhatsapp className="icon-contact" />
+        <TbFileCv className="icon-contact" />
       </section>
     </article>
   );
